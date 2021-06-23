1c70ec67201f8b7f743273dc9b91b447228ccbca
Sumanshu Arora <sumanshusamarora@gmail.com>
2021-06-23 16:38:43 +1000

    Handles SNS message

--- django_ocr_service/ocr/api.py
+++ django_ocr_service/ocr/api.py
@@ -167,25 +167,27 @@ class GenerateOCR_SNS(APIView):
                         status=status.HTTP_400_BAD_REQUEST
                     )
             else:
+                #{
+                #    'Type': 'Notification', 'MessageId': '549ab8c1-b1e2-5437-81c1-e18d60b685b8',
+                #    'TopicArn': 'arn:aws:sns:us-west-2:287646123622:uploaded-file-event-topic', 'Subject': 'input-record',
+                #     'Message': '{\n    "id": "5d5da358-87b4-499d-bf95-74fc8e785387",\n    "source": "file-upload-handler",\n    "type": "file.upload.v1",\n    "data": {\n        "awsRegion": "us-west-2",\n        "bucket": "shore-dev-bucket",\n        "fileKey": "Initial%25201003%2520-%2520Alley.pdf",\n        "uri": "s3://shore-dev-bucket/Initial%25201003%2520-%2520Alley.pdf"\n    }\n}',
+                #     'Timestamp': '2021-06-23T06:05:08.186Z', 'SignatureVersion': '1',
+                #     'Signature': 'Ybsiw8FdXcTA7o/eQa4rrwV1xGaz7Ow4O3+CdbFBKDz8Cfv167OqRkLY0J1vN3e748/yNIT8FnMT58NQvBwPQvnbqOeWfj/C5rMRRxe/L9tE3yq20TIp/21jTXHUG6/GNyYZbhIsL5M3S7h0WPbNnI75MX6gf5KErpoBVXHoGKSzY9w+HQJ/Dnrr7UBtv7+Y2t0AlboN2sdupRZDF4R9F+pvPX/0hNXw+utvwpYfMUID/HlQcWme46OYHPc+ZcUnNZPhki9VAU82rviCQG1OYIk8Xlsx0eL4MMSSczaOI8DKhMk1Cm4vCjwCcnnGRbJQ4KBl0MpaR0U9YetRIIbizQ==',
+                #     'SigningCertURL': 'https://sns.us-west-2.amazonaws.com/SimpleNotificationService-010a507c1833636cd94bdb98bd93083a.pem',
+                #     'UnsubscribeURL': 'https://sns.us-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-west-2:287646123622:uploaded-file-event-topic:93a88c57-7447-432b-b900-37520423bccf'
+                 #}
                 # The actual body is in the 'Message' key of the
                 # notification, which in turn is also a json encoded
                 # string. Which needs to be parsed as json before
                 # actually being useful.
                 message = json.loads(payload.get('Message'))
-                logger.info('SNS Notification received', extra=dict(
-                    message_type=message_type,
-                    sns_payload=request.body,
-                    payload_message=message,
-                ))
-
                 return self.handle_sns_message(message)
 
             return Response(status=status.HTTP_200_OK)
 
     def handle_sns_message(self, message):
-
         logger.info(message)
-
-        return Response(status=status.HTTP_200_OK)
+        model_obj = OCRInput.objects.create(cloud_storage_url_or_uri=message["data"]["uri"])
+        return Response(data={"guid":model_obj.guid}, status=status.HTTP_200_OK)
 
 
