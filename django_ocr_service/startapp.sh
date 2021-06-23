006061daf6b3491c948bce1e97c6c74a5d4227b9
Sumanshu Arora <sumanshusamarora@gmail.com>
2021-06-23 11:18:44 +1000

    Creating superuser at app start

--- django_ocr_service/startapp.sh
+++ django_ocr_service/startapp.sh
@@ -1,3 +1,4 @@
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --no-input
 gunicorn django_ocr_service.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080 -t 300 --log-level INFO
