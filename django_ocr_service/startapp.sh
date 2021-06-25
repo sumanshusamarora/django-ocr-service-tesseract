e99f414be4989ba4937b2b9da0299e09ed3d099e
Sumanshu Arora <sumanshusamarora@gmail.com>
2021-06-25 16:52:28 +1000

    Start qcluster in startapp.sh

--- django_ocr_service/startapp.sh
+++ django_ocr_service/startapp.sh
@@ -1,4 +1,5 @@
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py qcluster
 python manage.py createsuperuser --no-input
 gunicorn django_ocr_service.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080 -t 300 --log-level INFO
