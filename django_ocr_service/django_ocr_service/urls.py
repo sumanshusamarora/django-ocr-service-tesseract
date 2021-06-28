f5df6c4ff78548f8f994b57d4cad8da40d18d457
Sumanshu Arora <sumanshusamarora@gmail.com>
2021-06-28 17:08:07 +1000

    Adding scheduler code doesnt play along nicely in apps.py so moved to urls.py so it runs only once at app start

--- django_ocr_service/django_ocr_service/urls.py
+++ django_ocr_service/django_ocr_service/urls.py
@@ -13,6 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import logging
+
+import arrow
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,6 +27,8 @@ from ocr.api import (
     GenerateToken,
 )
 
+logger = logging.getLogger(__name__)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
@@ -31,3 +37,36 @@ urlpatterns = [
     path("api/get-ocr/", GetOCR.as_view()),
     path("api/sns/ocr/", GenerateOCR_SNS.as_view()),
 ]
+
+
+
+from django_q.models import Schedule
+from django_q.tasks import schedule
+
+schedule_task_name = "CleanUpStorage"
+
+# Delete all existing storage clean task
+try:
+    Schedule.objects.filter(name=schedule_task_name).delete()
+
+    # Add new task to clean storage
+    _ = schedule(
+        name=schedule_task_name,
+        func="ocr.ocr_utils.clean_local_storage",
+        schedule_type=Schedule.DAILY,
+        dirpath=settings.LOCAL_FILES_SAVE_DIR,
+        days=settings.DELETE_OLD_IMAGES_DAYS,
+        q_options={
+            "ack_failure": True,
+            "catch_up": False,
+            "max_attempts": 1,
+        },
+        next_run=arrow.utcnow().shift(days=1).replace(hour=10, minute=0).datetime,
+    )
+
+    logger.info("Storage cleaning task schedule created!!!")
+except Exception as exception:
+    logger.error(f"{schedule_task_name} task scheduling failed - {exception}")
+
+
+
--- django_ocr_service/ocr/apps.py
+++ django_ocr_service/ocr/apps.py
@@ -1,12 +1,7 @@
 """
 
 """
-import logging
-
-import arrow
 from django.apps import AppConfig
-
-logger = logging.getLogger(__name__)
 
 class OcrConfig(AppConfig):
     """
@@ -14,35 +9,4 @@ class OcrConfig(AppConfig):
     """
     default_auto_field = "django.db.models.BigAutoField"
     name = "ocr"
-
-    def ready(self):
-        """
-        Schedule the task to
-        :return:
-        """
-        from django.conf import settings
-        from django_q.models import Schedule
-        from django_q.tasks import schedule
-
-        schedule_task_name = "CleanUpStorage"
-
-        # Delete all existing storage clean task
-        Schedule.objects.filter(name=schedule_task_name).delete()
-
-        # Add new task to clean storage
-        _ = schedule(
-            name=schedule_task_name,
-            func="ocr.ocr_utils.clean_local_storage",
-            schedule_type=Schedule.DAILY,
-            dirpath=settings.LOCAL_FILES_SAVE_DIR,
-            days = settings.DELETE_OLD_IMAGES_DAYS,
-            q_options={
-                "ack_failure": True,
-                "catch_up": False,
-                "max_attempts": 1,
-            },
-            next_run=arrow.utcnow().shift(days=1).replace(hour=10, minute=0).datetime,
-        )
-
-        logger.info("Storage cleaning task schedule created!!!")
 
