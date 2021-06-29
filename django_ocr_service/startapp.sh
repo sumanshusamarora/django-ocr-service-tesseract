9cab3f3c009d77c6bfbbe43e63bb5647ae626360
Sumanshu Arora <sumanshusamarora@gmail.com>
2021-06-29 18:18:58 +1000

    Changes service start order

--- django_ocr_service/startapp.sh
+++ django_ocr_service/startapp.sh
@@ -2,5 +2,5 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations django_q
 python manage.py migrate
 python manage.py createsuperuser --no-input
-python manage.py qcluster &
-gunicorn django_ocr_service.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080 -t 300 --log-level INFO
+gunicorn django_ocr_service.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8080 -t 300 --log-level INFO &
+python manage.py qcluster
